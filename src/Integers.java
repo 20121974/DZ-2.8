83cd1e61054c4ef3dda05d3265d3355506222a24
20121974 <nfi46102@mail.ru>
2022-10-26 14:24:24 +0300

    3 commit

--- src/Integers.java
+++ src/Integers.java
@@ -3,15 +3,14 @@ import java.util.List;
 import java.util.Random;
 
 public class Integers {
-    List<Integers> integers = new LinkedList<>();
+    List<Integers> integers = new LinkedList<>(20);
 
     public List<Integers> getIntegers() {
         return integers;
     }
 
     int array[]=new int[20];
-
-        for (int i=0; i < array.length ; i++) {
+        for(int i=0; i < array.length ; i++) {
         Random random = null;
         array[i] = random.nextInt(1000);
         System.out.println(array[i]);

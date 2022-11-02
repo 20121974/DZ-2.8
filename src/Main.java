7620f807fe21eaa294a405acf3bcadfa6f4c59ea
20121974 <nfi46102@mail.ru>
2022-11-02 19:23:55 +0300

    12 commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
 import java.util.*;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-
-
+        System.out.println(("ДЗ 2.2"));
+        Random random = new Random();
+        List<Integer> number = new ArrayList<>(20);
+        for (int i = 0; i < 20; i++) {
+            number.add(random.nextInt(1000));
+        }
+        System.out.println(number);
+        for (int i = 0; i < number.size(); i++) {
+            if (number.get(i) % 2 !=0){
+                number.remove(i);
+                i = -1;
+            }
+        }System.out.println(number);
     }
 }
